50122417f57308997598b54188664e142037dd26
Sean Murphy <sdmurphy.1999@gmail.com>
2022-03-22 15:57:45 -0400

    initial commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Sean Murphy on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
